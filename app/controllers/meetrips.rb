465ae6ce709df1a19075932b1fa9954477b71bf5
Kye Bracey <kye.bracey@gmail.com>
2017-07-04 14:43:24 +0100

    Update meetrips.rb
    Removes get / route (tester)

--- app/controllers/meetrips.rb
+++ app/controllers/meetrips.rb
@@ -1,9 +1,5 @@
 class Meeto < Sinatra::Base
 
-  get '/' do
-    "HEllo worl!"
-  end
-
   post '/meetrips/create' do
     meetrip = Meetrip.create({ from_location: params[:from_location],
                                to_location:   params[:to_location],
