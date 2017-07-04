a1a7ee901aace5f2c2f6e8719c4e788b9bf6ddd5
Kynosaur <kye.bracey@gmail.com>
2017-07-04 14:39:17 +0100

    Fix typos

--- app/controllers/meetrips.rb
+++ app/controllers/meetrips.rb
@@ -5,10 +5,10 @@ class Meeto < Sinatra::Base
   end
 
   post '/meetrips/create' do
-    @meetrip = Meetrip.create({ from_location: params[:from_location],
-                     to_location:   params[:to_location],
-                     contact_info:  params[:contact_info] })
-    @meetrip.to_json
+    meetrip = Meetrip.create({ from_location: params[:from_location],
+                               to_location:   params[:to_location],
+                               contact_info:  params[:contact_info] })
+    meetrip.to_json
   end
 
 end
--- spec/features/creating_new_meetrips_spec.rb
+++ spec/features/creating_new_meetrips_spec.rb
@@ -1,9 +1,10 @@
 feature 'Meetrips' do
-  scenario 'Meetrips can be created and saved to the database via the controller' do
+  scenario 'can be created and saved to the database via the controller' do
     meetrip = post '/meetrips/create', from_location: 'London',
                                        to_location: 'Manchester',
                                        contact_info: '555 0123'
     expect(meetrip.status).to eq(200)
+    p meetrip
     expect(JSON.parse(meetrip.body)).to eq({"id"=>1,
                                             "from_location"=>"London",
                                             "to_location"=>"Manchester",
